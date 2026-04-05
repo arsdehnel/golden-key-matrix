a7ccf61373ca9dbc7ca3ea8fe430c170c9859e19
Adam Dehnel <arsdehnel@gmail.com>
2026-04-05 14:16:24 -0500

    feat: add homepage and nav to get to OSN

---
+++ src/pages/osn/index.tsx
@@ -0,0 +1,15 @@
+export default function Pages__OSN__Index() {
+	return (
+		<>
+			<h1>GKM OSN</h1>
+			<ul>
+				<li>
+					<a href="/osn/welcome">Welcome Slide / Poll</a>
+				</li>
+				<li>
+					<a href="/osn/thank-you">Thank You Slide / Leavebehinds</a>
+				</li>
+			</ul>
+		</>
+	);
+}
