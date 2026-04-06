da42ff36238301079938301c66dcb13068f46f2c
Adam Dehnel <arsdehnel@gmail.com>
2026-04-05 23:35:33 -0500

    chore: more linting fixes

--- src/pages/osn/routes.ts
+++ src/pages/osn/routes.ts
@@ -1,7 +1,7 @@
 import { route } from "rwsdk/router";
 import Pages__OSN__Index from "./index";
-import Pages__OSN__Welcome from "./welcome";
 import Pages__OSN__ThankYou from "./thank-you";
+import Pages__OSN__Welcome from "./welcome";
 
 export default [
 	route("/", Pages__OSN__Index),
