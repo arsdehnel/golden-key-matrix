b88b7d6b76d6553c9268c4dc0720693ed9b70c07
Adam Dehnel <arsdehnel@gmail.com>
2026-04-10 18:15:12 -0500

    feat: more pieces of the matrix

--- src/pages/home.tsx
+++ src/pages/home.tsx
@@ -6,11 +6,7 @@ export default function Pages__Home() {
 			<p>
 				The Golden Key Matrix started as a brainstorming idea for a talk about UX/Design and Architecture working together
 				to build amazing products. The public introduction of the concepts is happening at Open Source North 2026 in St
-				Paul, Minnesota.
-			</p>
-			<h2>OSN Talk</h2>
-			<p>
-				<a href="/osn">OSN Items</a>
+				Paul, Minnesota under the title <u>The Golden Key to Building Badass Products</u>.
 			</p>
 		</StandardLayout>
 	);
--- src/pages/matrix.tsx
+++ src/pages/matrix.tsx
@@ -13,11 +13,22 @@ export default function Pages__Matrix() {
 				<div className="matrix-key-contents-container">
 					<div className="matrix-key-contents">
 						<p>Being open to learning about the other side is key</p>
+						<ul>
+							<li>Bullet about open mindset</li>
+							<li>Another bullet about open mindset</li>
+							<li>Another bullet about open mindset</li>
+							<li>Another bullet about open mindset</li>
+							<li>Another bullet about open mindset</li>
+							<li>Another bullet about open mindset</li>
+						</ul>
+						<p>Wrap up words about open mindset</p>
 					</div>
-					<img
-						src="/dev-design-quadrant.jpg"
-						alt="Empty quadrant graph showing Design on the x-axis and Development on the y-axis"
-					/>
+					<div className="matrix-key-quadrant">
+						<img
+							src="/dev-design-quadrant.jpg"
+							alt="Empty quadrant graph showing Design on the x-axis and Development on the y-axis"
+						/>
+					</div>
 				</div>
 			</div>
 			<div className="matrix-key">
@@ -26,10 +37,12 @@ export default function Pages__Matrix() {
 					<div className="matrix-key-contents">
 						<p>Iteration, back-and-forth, and near-constant collaboration</p>
 					</div>
-					<img
-						src="/empty-quadrant.jpg"
-						alt="Empty quadrant graph showing Design on the x-axis and Development on the y-axis"
-					/>
+					<div className="matrix-key-quadrant">
+						<img
+							src="/dev-design-quadrant.jpg"
+							alt="Empty quadrant graph showing Design on the x-axis and Development on the y-axis"
+						/>
+					</div>
 				</div>
 			</div>
 			<div className="matrix-key">
@@ -38,12 +51,18 @@ export default function Pages__Matrix() {
 					<div className="matrix-key-contents">
 						<p>Documentation, microsites, videos, tutorials, and word-of-mouth</p>
 					</div>
-					<img
-						src="/empty-quadrant.jpg"
-						alt="Empty quadrant graph showing Design on the x-axis and Development on the y-axis"
-					/>
+					<div className="matrix-key-quadrant">
+						<img
+							src="/dev-design-quadrant.jpg"
+							alt="Empty quadrant graph showing Design on the x-axis and Development on the y-axis"
+						/>
+					</div>
 				</div>
 			</div>
+			<p>
+				Fill this out on a regular basis. Maybe it's a per-sprint item or every-other sprint but something that keeps it
+				top of mind. And <strong>be honest</strong> because nobody will benefit from fake and insincere answers.
+			</p>
 		</StandardLayout>
 	);
 }
