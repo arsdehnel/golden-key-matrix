5a89ff880046a6d34d35f8f81e999dd9c14df471
Adam Dehnel <arsdehnel@gmail.com>
2026-05-16 18:08:09 -0500

    feat: more styling for post-osn pages

--- src/pages/matrix.tsx
+++ src/pages/matrix.tsx
@@ -14,16 +14,16 @@ export default async function Pages__Matrix({ ctx }: RequestInfo): Promise<React
 				<h3>Key 1: Open Mindset</h3>
 				<div className="matrix-key-contents-container">
 					<div className="matrix-key-contents">
-						<p>Being open to learning about the other side is key</p>
+						<p>Creates trust in others, but also in yourself</p>
 						<ul>
-							<li>Bullet about open mindset</li>
-							<li>Another bullet about open mindset</li>
-							<li>Another bullet about open mindset</li>
-							<li>Another bullet about open mindset</li>
-							<li>Another bullet about open mindset</li>
-							<li>Another bullet about open mindset</li>
+							<li>Receptive to new ideas, experiences, and perspectives</li>
+							<li>Fuels adaptability & flexibility</li>
+							<li>Embraces challenges as opportunities</li>
+							<li>Respect for others</li>
+							<li>Curious & asks questions</li>
+							<li>Receives questions as ideas not criticism</li>
 						</ul>
-						<p>Wrap up words about open mindset</p>
+						<p>Sets yourself up to learn</p>
 					</div>
 					<div className="matrix-key-quadrant">
 						<GkmQuadrant
@@ -31,7 +31,7 @@ export default async function Pages__Matrix({ ctx }: RequestInfo): Promise<React
 							xAxisTitle="Design"
 							yAxisInfinityLabel="Expert"
 							xAxisInfinityLabel="Expert"
-							sharedAxisZeroLabel="None"
+							sharedAxisZeroLabel="Basic"
 						/>
 					</div>
 				</div>
@@ -40,6 +40,13 @@ export default async function Pages__Matrix({ ctx }: RequestInfo): Promise<React
 				<h3>Key 2: Integrated Workflows</h3>
 				<div className="matrix-key-contents-container">
 					<div className="matrix-key-contents">
+						<p>Ideas to get on the same wavelength!</p>
+						<ul>
+							<li>Prototype run throughs</li>
+							<li>UX Intention Discussions</li>
+							<li>Dev Build Explanations</li>
+							<li>Cross-team paired programming</li>
+						</ul>
 						<p>Iteration, back-and-forth, and near-constant collaboration</p>
 					</div>
 					<div className="matrix-key-quadrant">
@@ -48,16 +55,25 @@ export default async function Pages__Matrix({ ctx }: RequestInfo): Promise<React
 							xAxisTitle="Design"
 							yAxisInfinityLabel="Expert"
 							xAxisInfinityLabel="Expert"
-							sharedAxisZeroLabel="None"
+							sharedAxisZeroLabel="Basic"
 						/>
 					</div>
 				</div>
 			</div>
 			<div className="matrix-key">
-				<h3>Key 3: Document & Share</h3>
+				<h3>Key 3: Rich Ecosystem</h3>
 				<div className="matrix-key-contents-container">
 					<div className="matrix-key-contents">
-						<p>Documentation, microsites, videos, tutorials, and word-of-mouth</p>
+						<p>Starting can be the hardest part</p>
+						<ul>
+							<li>Prioritize what's needed first</li>
+							<li>Find the medium(s) that makes the most sense for your teams</li>
+							<li>Consider timing/cadence</li>
+							<li>Try, try again…</li>
+							<li>Be accountable</li>
+							<li>Set up Ownership</li>
+						</ul>
+						<p>Continue the effort…</p>
 					</div>
 					<div className="matrix-key-quadrant">
 						<GkmQuadrant
@@ -65,7 +81,7 @@ export default async function Pages__Matrix({ ctx }: RequestInfo): Promise<React
 							xAxisTitle="Design"
 							yAxisInfinityLabel="Expert"
 							xAxisInfinityLabel="Expert"
-							sharedAxisZeroLabel="None"
+							sharedAxisZeroLabel="Basic"
 						/>
 					</div>
 				</div>
